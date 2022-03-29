98504b9301bfd629dcaf44ac5d3e9d6fb4acaf5b
Bruno Lemes <bruno.santos@climatempo.io>
2022-03-29 16:53:03 -0300

    Exemplo: Codigo criado de exemplo

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const arrow = () => {};
+  var dsdsdddddddd;
+
+  function depurar() {
+    console.log("dsdddsddddddsdsddsds");
+  }
+
+  return (
+    <div className="App">
+      {depurar()}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div></div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
